3840dd59aeabddc64dfc5d584ba62168041ad86d
teinnsei <alexander.galagutskiy@pm.me>
2021-04-14 15:00:23 -0500

    isntall-hsdis.sh

---
+++ README.md
@@ -0,0 +1,11 @@
+# hsdis
+
+## Install hsdis
+
+### Linux and Mac OS X
+```bash
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Cronenberg-World/hsdis/HEAD/install-hsdis.sh)"
+```
+
+### Windows
+Support will be add in the next release
---
+++ install-hsdis.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+current_dir=$(pwd)
+#hash=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+work_dir="/tmp/hsdis-install"
+
+if [ -z "$JAVA_HOME" ]; then
+  echo "JAVA_HOME is not set"
+  exit 1
+fi
+
+if [ ! -d "$work_dir" ]; then
+  mkdir $work_dir
+  cd $work_dir
+  curl -L -s https://github.com/oracle/graal/archive/refs/heads/master.zip > master.zip
+  unzip master.zip 1>/dev/null
+  cd graal-master
+  curl -L -s https://github.com/graalvm/mx/archive/refs/heads/master.zip > mx.zip
+  unzip mx.zip 1>/dev/null
+fi
+
+cd $work_dir/graal-master/compiler
+/bin/bash ../mx-master/mx hsdis
+
+cd $current_dir
